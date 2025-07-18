cf6b847787b6bf9b591840aa0be9f23d72c91723
todaysfinds <todaysfindss@gmail.com>
2025-07-18 20:19:17 +0900

    Deploy minimal working version - Flask only

---
+++ app_simple.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>🎉 성공! 독서모임 앱이 작동합니다!</h1><p>이제 기능을 추가할 준비가 되었습니다.</p>"
+
+@app.route('/test')
+def test():
+    return "<h1>테스트 성공!</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
